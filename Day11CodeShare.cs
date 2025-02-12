97533ef45b132eda4808425cf2c81ec6a0f89a15
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-12 09:15:10 +0530

    srp demo

--- Day11CodeShare.cs
+++ Day11CodeShare.cs
@@ -19,3 +19,81 @@ D: Dependency Inversion Principle (DIP)
 
 
 OOAD means object oriented analysis and design here design means DRY ,divide and conquer and change of request 
+
+
+
+Single Responsibility Principle (SRP) - Real-Time Example in C#
+-------------------------------------------------------------------
+The Single Responsibility Principle (SRP) is one of the SOLID principles, which states that a class should have only one reason to change, meaning it should only have one job or responsibility. If a class has more than one responsibility, it can lead to code that is difficult to maintain and modify.
+
+
+create one folder Day11Projects 
+
+
+and write the first commnd like this 
+
+dotnet new console -o srpdemo --use-program-main
+
+add one class into the projet right click and c# and select class and drop down write Person and press enter then namespaes will come automatically 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace srpdemo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
+and in the main program the code is like this 
+
+namespace srpdemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to my application!");
+
+        // Ask for user information
+        Person user = new Person();
+
+        Console.Write("What is your first name: ");
+        user.FirstName = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        user.LastName = Console.ReadLine();
+
+        // Checks to be sure the first and last names are valid
+        if (string.IsNullOrWhiteSpace(user.FirstName))
+        {
+            Console.WriteLine("You did not give us a valid first name!");
+            Console.ReadLine();
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(user.LastName))
+        {
+            Console.WriteLine("You did not give us a valid last name!");
+            Console.ReadLine();
+            return;
+        }
+
+        // Create a username for the person
+        Console.WriteLine($"Your username is {user.FirstName.Substring(0, 1)} {user.LastName}");
+
+        Console.WriteLine("Press Enter to Close the Application ");
+        Console.ReadLine();
+
+
+    }
+}
+
+
+Now i am dividing the responsiblities like this into seperate classes means i am adding some classes like how i added Person class
+
