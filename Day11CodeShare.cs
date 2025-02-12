8811c18631d9f15c60f9ca3a69bced672d7fee36
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-12 08:52:06 +0530

    initaltheory

--- Day11CodeShare.cs
+++ Day11CodeShare.cs
@@ -1,1 +1,21 @@
 
+Developers build applications with good and tidy designs using their knowledge and experience. But over time, applications 
+  might develop bugs. The application design must be altered for every change request or new feature request. 
+  After some time, we might need to put in a lot of effort, even for simple tasks, and it might require a full working knowledge of the entire system. But we can't blame change or new feature requests. They are part of software development. We can't stop them or refuse them either. So who is the culprit here? It is the design of the application.
+
+The following are the design flaws that cause damage to software, mostly.
+
+Putting more stress on classes by assigning more responsibilities to them. (A lot of functionality not related to a class.)
+Forcing the classes to depend on each other. If classes depend on each other (in other words, tightly coupled), then a change in one will affect the other.
+Spreading duplicate code in the system/application.
+
+SOLID principles are the design principles that enable us to manage several software design problems. Robert C. Martin compiled these principles in the 1990s. These principles provide us with ways to move from tightly coupled code and little encapsulation to the desired results of loosely coupled and encapsulated real business needs properly. SOLID is an acronym for the following.
+
+S: Single Responsibility Principle (SRP)
+O: Open-closed Principle (OCP)
+L: Liskov substitution Principle (LSP)
+I: Interface Segregation Principle (ISP)
+D: Dependency Inversion Principle (DIP)
+
+
+OOAD means object oriented analysis and design here design means DRY ,divide and conquer and change of request 
