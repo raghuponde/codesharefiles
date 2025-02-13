331fd4ce9454a6d38335b8e098efab66332ef066
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-13 12:39:47 +0530

    Update Day12SqlNotes1.sql

--- Day12SqlNotes1.sql
+++ Day12SqlNotes1.sql
@@ -17,6 +17,11 @@ insert into Student(location,studname,studid) values('chennai','senthil',102);
 
 insert into student(studname,studid) values('shanthi',103);--forgetting error 
 
+--where value is forgetten there db will put null value what is null here unknown value it is not zero 
 
+-- you cannot forget primary key column 
+
+---multiple insert 
+insert into Student values(104,'sunitha','delhi'),(105,'rajesh','patna'),(106,'junaid','hyderabad')
 
 
