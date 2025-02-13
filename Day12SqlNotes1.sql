a853394bc8e784aafd7ab77ce0a2e107da1635d8
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-13 12:33:54 +0530

    dd

--- Day12SqlNotes1.sql
+++ Day12SqlNotes1.sql
@@ -4,4 +4,19 @@ use Wipro4;
 create table Student (studid int primary key ,
 studname varchar(30),location varchar(40));
 
+select * from Student;
+
+--normal insert 
+
+insert into Student values(101,'ravi','bangalore');
+
+-- inserting as per my order 
+insert into Student(location,studname,studid) values('chennai','senthil',102);
+
+--partial insert 
+
+insert into student(studname,studid) values('shanthi',103);--forgetting error 
+
+
+
 
