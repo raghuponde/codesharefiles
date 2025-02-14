40ef12ecc70cfd44ebd6dfd1b32afe0685ea56ad
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-14 12:28:57 +0530

    Update Day13sqlNotes.sql

--- Day13sqlNotes.sql
+++ Day13sqlNotes.sql
@@ -453,4 +453,64 @@ END
 
 select * from Employeedata ;
 
+Functions
+------------
+Functions in sql server :
+----------------------------------
+Function will always return a value but we have seen that a 
+stored procedure may or may not return a value .
+we have already used many functions in sql server 
+like sqrt,sum square ,substring like that but these are system defined
+functions ...so now we are going to create 
+
+User Defined functions: The function which are created by end user
+is called as user defined  functions .When you cant find a built-in 
+function that  meets your needs, you can write your own.
+
+UDFs come in three flavors: 
+1)scalar functions
+2)in-line table valued functions
+3) multi-statement table valued functions. 
+
+Function is always used with select clause....
+
+syntax :
+-------
+
+create function <function_name> ( paramters_list)
+returns <function_type>
+as
+begin
+
+------
+return ;
+------
+end
+
+execution : select <function_name> from <table_name>
+
+Scalar function:
+------------------
+scalar functions accept a  parameter and return a single 
+data value of type specified in RETURNS clause
+A scalar function can return any data type except
+text,ntext,image,cursor and timestamp.
+
+CREATE FUNCTION <function_name>
+(@input_variables  type)
+RETURNS data_type of result returned by function
+AS
+BEGIN
+.....  SQL Statements
+    RETURN (data_value)
+END
+
+select <function_name>
+
+
+
+
+
+
+
 
