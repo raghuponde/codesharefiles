ad7f676b4b0c251f8c472435d12ba1d3c27a1f2e
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-15 09:17:14 +0530

    Update Day14SqlNotes.sql

--- Day14SqlNotes.sql
+++ Day14SqlNotes.sql
@@ -1,2 +1,18 @@
+right click on database and say restore dataabse 
+
+click device and three dots 
+
+then say add 
+
+copy the path from the above highlighted text box into the notepad only the path u copy 
+
+C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup  
+
+after copying the path close all widows 
+
+and in that path u put these two .bak files 
+
+then again follow the same steps and select the database and say ok ok it will be installed 
+
 
  
