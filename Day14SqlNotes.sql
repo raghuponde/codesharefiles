1cfcbde51483bb473bbe690107136046a6865316
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-15 10:06:05 +0530

    Update Day14SqlNotes.sql

--- Day14SqlNotes.sql
+++ Day14SqlNotes.sql
@@ -175,4 +175,47 @@ select * from id
 select id,count(id) as 'count' from id group by id -- here one columnis there so used as per requirmnt 
  
 
+create table dept1(
+  deptno int ,
+  dname  varchar(14),
+  loc    varchar(13),
+  constraint pkk1 primary key(deptno)
+);
+ 
+ create table emp1(
+  empno    int primary key,
+  ename    varchar(10),
+  job      varchar(9),
+  mgr      int,
+  hiredate date,
+  sal      int,
+  comm     int,
+  deptno   int  foreign key  references dept1 (deptno)
+);
 
+insert into dept1 values(10, 'ACCOUNTING', 'NEW YORK');
+insert into dept1 values(20, 'RESEARCH', 'DALLAS');
+insert into dept1
+values(30, 'SALES', 'CHICAGO');
+insert into dept1
+values(40, 'OPERATIONS', 'BOSTON'); 
+
+select * from dept1;
+insert into emp1 values( 7839, 'KING', 'PRESIDENT', null,'1981-11-17' , 5000, null, 10);
+insert into emp1 values( 7698, 'BLAKE', 'MANAGER', 7839,'1981-05-01',2850, null, 30);
+insert into emp1 values( 7782, 'CLARK', 'MANAGER', 7839,'1981-06-09', 2450, null, 10);
+insert into emp1 values( 7566, 'JONES', 'MANAGER', 7839,'1981-04-02', 2975, null, 20);
+insert into emp1 values( 7788, 'SCOTT', 'ANALYST', 7566,'1987-04-19', 3000, null, 20);
+insert into emp1 values( 7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000, null, 20);
+insert into emp1 values( 7369, 'SMITH', 'CLERK', 7902,'1980-12-17', 800, null, 20);
+insert into emp1 values( 7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600, 300, 30);
+insert into emp1 values(  7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250, 500, 30);
+insert into emp1 values( 7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30);
+insert into emp1 values(  7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500, 0, 30);
+insert into emp1 values( 7876, 'ADAMS', 'CLERK', 7788, '1987-05-23', 1100, null, 20);
+insert into emp1 values( 7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950, null, 30);
+insert into emp1 values( 7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300, null, 10);
+
+
+select * from dept1;
+select * from emp1;
