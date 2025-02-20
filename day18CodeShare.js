4e6234463ded86891520aa48b33007e2401e364e
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-20 10:12:14 +0530

    Update day18CodeShare.js

--- day18CodeShare.js
+++ day18CodeShare.js
@@ -331,8 +331,25 @@ const ids = people.map(person => person.id);
 console.log(ids);
 
 
+ formula for filter  
+----------------------
+ 
+array.filter((element)=>(condition))
+
+var arr2 = [10, 20, 30, 40, 50]
+let filtered = arr2.filter((element) => element > 20);
+console.log(filtered);
+
+const people2 = [
+{ id: 1, name: 'Felipe', country: 'DR' },
+{ id: 2, name: 'Scott', country: 'USA' },
+{ id: 3, name: 'Jennifer', country: 'Canada' },
+  { id: 4, name: 'Marry', country: 'USA' }
+]
 
 
+const peoplesinUSA = people2.filter(person => person.country=='USA');
+console.log(peoplesinUSA);
 
 
 
