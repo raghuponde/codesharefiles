30906755b344dfd5312f10e85c2adb2e0da149ee
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-21 09:20:59 +0530

    Update Day19CodeShare.js

--- Day19CodeShare.js
+++ Day19CodeShare.js
@@ -85,3 +85,35 @@ background-color: #0F3460;
 }
 
 
+App.js 
+---------
+ 
+import './App.css';
+
+function App() {
+  let count = 0;
+
+  function handleAdd()
+  {
+    count = count + 1;
+    console.log(count);
+  }
+
+
+  return (
+    <div className="App">
+
+      <div className='box'>
+
+        <p>{count}</p>
+        <button onClick={handleAdd} className='add' >ADD</button>
+        <button className='sub'>SUB</button>
+
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
