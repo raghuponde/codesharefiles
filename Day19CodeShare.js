2558931c7c330be46173bea5d644e7869d471361
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-21 09:52:02 +0530

    Update Day19CodeShare.js

--- Day19CodeShare.js
+++ Day19CodeShare.js
@@ -372,6 +372,57 @@ function App() {
 
 export default App;
 
+now i can also use PrevState variabel also in the coede 
+
+App.js
+--------
+ 
+import { useState } from 'react';
+import './App.css';
+
+
+function App() {
+
+  const [count,setCount] = useState(0);
+
+  function handleAdd()
+  {
+    //count = count + 1;
+    console.log(count);
+    setCount(prevCount=>prevCount + 1);
+     setCount(prevCount=>prevCount + 1);
+      setCount(prevCount=>prevCount + 1);
+  }
+
+  // function handleSub()
+  // {
+  //   setCount(count - 1);
+  // }
+
+  function handleReset()
+  {
+    setCount(0);
+  }
+ 
+
+  return (
+    <div className="App">
+
+      <div className='box'>
+
+        <p>{count}</p>
+        <button onClick={handleAdd} className='add' >ADD</button>
+        <button onClick={()=>setCount(count-1)} className='sub' >SUB</button>
+        <button onClick={handleReset} className='reset' >RESET</button>
+
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
 
 
 
