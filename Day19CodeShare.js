e952ea2f769b222d35bf6ee7f38465f2c6916543
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-21 08:58:12 +0530

    Update Day19CodeShare.js

--- Day19CodeShare.js
+++ Day19CodeShare.js
@@ -1,1 +1,14 @@
 
+State and UseState in react 
+---------------------------
+The React useState Hook allows us to track state in a function component.
+State generally refers to data or properties that need to be tracking in an application.
+We initialize our state by calling useState in our function component.
+useState accepts an initial state and returns two values:
+The current state.
+A function that updates the state.
+
+create an new app  with the name counterdemo in Day19 folder and close the vscode and again open that app folder using vscode only 
+
+npx create=react-app counterdemo
+
