8806a9bdb9fc6bd06818d45ed6db78d6f0971eb0
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-21 15:05:20 +0530

    Update Day19CodeShare.js

--- Day19CodeShare.js
+++ Day19CodeShare.js
@@ -2,7 +2,7 @@
 State and UseState in react 
 ---------------------------
 The React useState Hook allows us to track state in a function component.
-State generally refers to data or properties that need to be tracking in an application.
+State generally refers to data or properties that need to be tracking in an application. 
 We initialize our state by calling useState in our function component.
 useState accepts an initial state and returns two values:
 The current state.
