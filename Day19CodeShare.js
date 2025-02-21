951f3176e9be5a512cefceb85e27bf44fbfb5416
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-21 09:13:40 +0530

    Update Day19CodeShare.js

--- Day19CodeShare.js
+++ Day19CodeShare.js
@@ -27,3 +27,26 @@ reportWebVitals();
 
 
 then add one <div> root tag surorind <App/>
+then in App.js 
+remove 
+
+ <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+also logo import 
+
+
+
+
