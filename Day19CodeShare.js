fd0e872e1f69782a5486c2cec08cc725fe803156
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-21 15:04:02 +0530

    Update Day19CodeShare.js

--- Day19CodeShare.js
+++ Day19CodeShare.js
@@ -1346,14 +1346,15 @@ export default function Taskcard(props) {
 }
 
 
-Now i had added two css files into the src folder and taken out the present TaskList.js i am moving it to src folder only 
+Now i had added two css files into the components folder and let the TaskList.js be in Components folder only 
 
  TaskList.js
 --------------
 import { useState } from 'react';
-import  Taskcard  from './components/Taskcard';
+import  Taskcard  from './Taskcard';
 import "./TaskList.css";
 import "./AddTask.css";
+import "./../App3.css"
 export const TaskList = () => {
 const [tasks, setTasks] = useState([
 {id: 5271, name: "Record React Lectures", completed: true},
@@ -1380,80 +1381,91 @@ return (
 }
 
 export default TaskList
-
 TaskList.css
 -------------
-.tasklist ul{
-max-width: 800px;
-margin: 20px auto;
-padding: 20px;
-box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
-border-radius: 5px;
-}
-.tasklist .header{
-display: flex;
-justify-content: space-between;
-align-items: center;
-margin: 20px 10px;
-margin-bottom: 30px;
-}
-.tasklist h1{
-font-size: 28px;
-text-align: center;
-}
-.tasklist button.trigger{
-border: 0px;
-border-radius: 5px;
-background-color: var(--theme-button);
-color: #FFFFFF;
-padding: 5px 10px;
-cursor: pointer;
-font-size: 16px;
-}
-AddTask.css
-------------
-.addtask{
-max-width: 800px;
-margin: 50px auto;
-padding: 20px;
-box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
-border-radius: 5px;
-}
-.addtask form{
-display: flex;
-justify-content: space-around;
-align-items: center;
-flex-wrap: wrap;
-}
-.addtask label{
-font-size: 20px;
-margin-right: 5px;
-}
-.addtask input{
-font-size: 18px;
-padding: 7px;
-border: 1px solid #878787;
-border-radius: 5px;
-flex-grow: 1;
-margin: 10px;
-}
-.addtask input:focus{
-outline: none;
-}
-.addtask select{
-font-size: 18px;
-padding: 7px;
-border-radius: 5px;
-margin: 10px;
-cursor: pointer;
-}
-.addtask button{
-font-size: 18px;
-padding: 7px 10px;
-margin: 10px;
+.tasklist ul {
+    max-width: 800px;
+    margin: 20px auto;
+    padding: 20px;
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+    border-radius: 5px;
 }
 
-update this your project 
+.tasklist .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px 10px;
+    margin-bottom: 30px;
+}
+
+.tasklist h1 {
+    font-size: 28px;
+    text-align: center;
+}
+
+.tasklist button.trigger {
+    border: 0px;
+    border-radius: 5px;
+    background-color: blue;
+    color: #FFFFFF;
+    padding: 5px 10px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+
+AddTask.css
+------------
+.addtask {
+    max-width: 800px;
+    margin: 50px auto;
+    padding: 20px;
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+    border-radius: 5px;
+}
+
+.addtask form {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+.addtask label {
+    font-size: 20px;
+    margin-right: 5px;
+}
+
+.addtask input {
+    font-size: 18px;
+    padding: 7px;
+    border: 1px solid #878787;
+    border-radius: 5px;
+    flex-grow: 1;
+    margin: 10px;
+}
+
+.addtask input:focus {
+    outline: none;
+}
+
+.addtask select {
+    font-size: 18px;
+    padding: 7px;
+    border-radius: 5px;
+    margin: 10px;
+    cursor: pointer;
+}
+
+.addtask button {
+    font-size: 18px;
+    padding: 7px 10px;
+    margin: 10px;
+}
+update this your project upto here now 
+
+
 
 
  
