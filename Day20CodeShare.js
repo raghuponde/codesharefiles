45f0abad89095ce9f320ddf20c47f92c5c79aacd
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-22 12:07:18 +0530

    Update Day20CodeShare.js

--- Day20CodeShare.js
+++ Day20CodeShare.js
@@ -692,4 +692,40 @@ export const ProductList = () => {
             )
 }
 
+now infinite loop issue is not coming 
+with display 
+-------------
+ import { useState ,useEffect} from "react"
+
+export const ProductList = () => {
+
+    
+          const [products, setProducts] = useState([]);
+         console.log(products);
+        useEffect(()=>{
+
+            fetch("http://localhost:8000/products")
+            .then(response => response.json())
+            .then(data => setProducts(data));
+         }, []);
+
+               return (
+         <section>
+                       {
+                           products.map((product) => (
+            <div className="card" key={product.id}>
+            <p className="id">{product.id}</p>
+            <p className="name">{product.name}</p>
+            <p className="info">
+            <span>${product.price}</span>
+            <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}
+            </span>
+            </p>
+            </div>
+)) }
+         </section>
+            )
+}
+
+
 
