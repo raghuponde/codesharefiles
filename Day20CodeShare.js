344cf15fc45a24eb1e7c68a90836d21dc588c4d5
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-22 14:38:39 +0530

    Update Day20CodeShare.js

--- Day20CodeShare.js
+++ Day20CodeShare.js
@@ -1035,10 +1035,76 @@ first take Header.js okay
 
 and go to getbootsrap.com and from there search nav bar and put it here 
 
+after putting i changed class to className and where style inline was there  had used {{ 
+
+Header.js
+ -----------
+ 
+ import React from 'react';
+
+export const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar scroll</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarScroll">
+          <ul
+            className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+            style={{ '--bs-scroll-height': '100px' }} // ✅ React style syntax
+          >
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About us</a>
+            </li>
+              <li className="nav-item">
+              <a className="nav-link" href="#">cryptogrpahy</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Link
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><div className="dropdown-divider"></div></li> {/* ✅ Replaced <hr> */}
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Link</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 
 
+and now i want a particular version only so
+npm i react-router-dom@6.4.3
 
+ 
 
 
 
