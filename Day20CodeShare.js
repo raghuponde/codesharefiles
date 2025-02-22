a081995584df27d87c8c2ff3fa0dcc13e2d18a74
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-22 12:30:57 +0530

    Update Day20CodeShare.js

--- Day20CodeShare.js
+++ Day20CodeShare.js
@@ -829,6 +829,50 @@ i will take count variable and put that count variable in [ count] that will dep
 
 see i had modified the code further by keeping All and in stock only to display 
 
+ProductLst.js 
+------------------
+ import { useState, useEffect } from "react"
+
+export const ProductList = () => {
+
+    const [products, setProducts] = useState([]);
+    const [counter, setCounter] = useState(0);
+    const [url,setUrl] = useState("http://localhost:8000/products");
+    console.log(products);
+    useEffect(() => {
+
+        fetch("http://localhost:8000/products")
+            .then(response => response.json())
+            .then(data => setProducts(data));
+    }, [url]);
+
+    useEffect(() => {
+        console.log(counter);
+    }, [counter]) //if  u modify this counter then only i will consoleit if counter is not there in array 
+    //then console .log will be zero onl u are doing it in first button click of counter 
+
+    return (
+
+        <section>
+            <div className="filter">
+                <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+                <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
+                <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock Only</button>
+            </div>
+            {products.map((product) => (
+                <div className="card" key={product.id}>
+                    <p className="id">{product.id}</p>
+                    <p className="name">{product.name}</p>
+                    <p className="info">
+                        <span>${product.price}</span>
+                        <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}
+                        </span>
+                    </p>
+                </div>
+            ))}
+        </section>
+    )
+}
 
 
 
