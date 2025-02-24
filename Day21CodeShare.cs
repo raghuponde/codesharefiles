9b72fa2d73cdd476138f117671f39198c57ca437
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-24 09:31:17 +0530

    Update Day21CodeShare.cs

--- Day21CodeShare.cs
+++ Day21CodeShare.cs
@@ -1,1 +1,47 @@
  
+open one visual studio project 2022 C#--- all plarforms-- web 
+
+select asp.net core web api 
+
+create a project .net 8.0 you can select 
+
+
+ so default controller will be there the code will look like this I had chnaged the url like this for method 
+
+ using Microsoft.AspNetCore.Mvc;
+
+namespace Webapidemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]  // base url 
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+    
+        [Route("weather1")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
+
+run and see the effect here how the url is changed 
