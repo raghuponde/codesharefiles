d0fa1f511da77a8ebe0718474b3e19867ebdb5d2
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-02-28 10:46:04 +0530

    Update Phase2Day1CodeShare.cs

--- Phase2Day1CodeShare.cs
+++ Phase2Day1CodeShare.cs
@@ -1,2 +1,33 @@
 
- 
+ Data sources means which provides data 
+
+Types of data sources :
+
+--->flat files like .txt,xml etc they provide data (file hanlding) 
+
+--->collection objects also contains data like array ,arraylist etc (programming like for loops and for each loop etc  )
+
+--->tables also contain data (to reqtrive data sql is used,ado.net etc )
+
+The same data u want to access from different data sources provided in easy way 
+then u will use linq 
+
+if u are using linq to access objects in memory objects then it is called linqtoobjects
+
+if u are using linq to access sql then it is called as linq to sql 
+
+thrid party softwares access linq to amazon is also there 
+
+
+
+The acronym LINQ stands for Language Integrated Query.
+
+Microsoft’s query language is
+fully integrated and offers easy data access from in-memory objects, databases, XML
+documents, and many more. 
+
+In your syllabus they have given only linq to objects so we will be doing demos only on this 
+
+
+syntax will be like sql way but select comes last and from comes first and in between where ,order by and other functionalities can be used .
+
