631662c746f36630d64046d513e8a93e725560ef
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 15:55:06 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -2005,7 +2005,21 @@ export class ProductList extends Component {
     }
 }
 
+  chnage in App.js file 
+  -------------------
+  import './App.css';
+//import { ProductList } from './ProductList';
+import { ProductList } from './ProductList2';
+function App()
+{
+        return (
+        <div className="App">
+        <ProductList />
+        </div>
+        );
+}
+export default App;
     
 
-
+now also the application will work withut any problem
 
