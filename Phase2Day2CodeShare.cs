7cefeeef3b5909e439c13f56c428611f5e892da2
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 15:21:59 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -1830,7 +1830,47 @@ export default class Counter extends React.Component {
         )
     }
 }
+next chceking previous state like we did in use state 
 
+import React from "react";
+export default class Counter extends React.Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.handleAttack = this.handleAttack.bind(this);
+        this.handleDefence = this.handleDefence.bind(this);
+        this.state = {
+            count:0
+        }
+    }
+
+    handleAttack()
+    {
+      
+        this.setState({ count: this.state.count + 1 })
+        this.setState({ count: this.state.count + 1 })
+        this.setState({ count: this.state.count + 1 })
+    }
+    handleDefence()
+    {
+       
+        this.setState({ count:this.state.count-1})
+    }
+    render() {
+        return (
+            <div className="row text-white">
+                <h1>Counter:{this.state.count} </h1>
+                <button  onClick={this.handleAttack} style={{ width: "200px" }}>+1</button>
+                <button onClick={this.handleDefence} style={{ width: "200px" }}>-1</button>
+            </div>
+        )
+    }
+}
+
+  above at a time 3 times it should increse but logic is not working so what to do 
+
+    
 
 
 
