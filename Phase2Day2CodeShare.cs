26345ad635386c39f5b72c8f32d2f48504f215cb
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 14:26:50 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -1568,5 +1568,27 @@ namespace CustomvalidationAndmoreTypesinValidation
 
     }
 }
+put it on dog class age 
+
+  using System.ComponentModel.DataAnnotations;
+
+namespace CRUDusingModel.Models
+{
+    public class Dog
+    {
+
+        [Required(ErrorMessage = "ID is required")]
+        public int ID { set; get; }
+
+        [Required(ErrorMessage = "Name is required"), MaxLength(222)]
+        public string? Name { set; get; }
+
+        [Required(ErrorMessage = "Age is required"),
+            Range(0, 20, ErrorMessage = "Age should be between 0 to 20 only")]
+        [EvenValidation]   // applied here 
+        public int Age { set; get; }
+
+    }
+}
 
  
