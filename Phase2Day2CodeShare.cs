890d71ccf9ae88217d39caa24354457f72688e72
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 12:04:21 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -694,6 +694,27 @@ Get and Post methods are there for only insert ,update and delete functionalitie
 
 now open a new application asp.net core give name CRUDusingModel
 
+in the models folder add one class like this 
+
+  using System.ComponentModel.DataAnnotations;
+
+namespace CRUDusingModel.Models
+{
+    public class Dog
+    {
+
+        [Required(ErrorMessage = "ID is required")]
+        public int ID { set; get; }
+
+        [Required(ErrorMessage = "Name is required"), MaxLength(222)]
+        public string? Name { set; get; }
+
+        [Required(ErrorMessage = "Age is required"),
+            Range(0, 20, ErrorMessage = "Age should be between 0 to 20 only")]
+        public int Age { set; get; }
+
+    }
+}
 
 
 
