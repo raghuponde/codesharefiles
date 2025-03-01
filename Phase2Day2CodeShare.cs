8a722959bcb33ec93c454e8a5bef28ea169a7a89
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 15:01:04 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -1686,7 +1686,20 @@ root.render(
   </React.StrictMode>
 );
 
-
+Now updated code of Counter.js 
+---------------------------------
+import React from "react";
+export default class Counter extends React.Component {
+render() {
+return (
+<div className="row text-white">
+<h1>Counter: </h1>
+<button style={{ width: "200px" }}>+1</button>
+<button style={{ width: "200px" }}>-1</button>
+</div>
+)
+}
+}
   
 
 
