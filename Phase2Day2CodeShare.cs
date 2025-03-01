e312e379010cb928f80e1767801181d0fa13dc60
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 11:59:56 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -679,6 +679,17 @@ earlier programtically i was filling the drop down now in desing only i am filli
 so when after doing frist click of register if i write properly and press tab to move it will go no need to press aagin register 
 button because i am doing clinet side valdiation also
 
+CRUD operation using a Model :
+-----------------------------------------
+Means for a model means for a class any class i want to define insert,update ,delete and read etc methods in Controller function means function for insert and function for update and all i want to write 
+here i will use get and post methods and all i will validate the class means business rules also i will apply 
+
+earlier manually i had gone into the view and written html code and embedded the model object s into it 
+but now i will ask the visual studio to genrate the code for me in the view that thing is called as scafffolding.
+
+so check program dog on this example ..
+
+Get and Post methods are there for only insert ,update and delete functionalities
 
 
 
