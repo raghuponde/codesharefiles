8236105386d141bbac8fb71a021653518cd8d1b2
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 15:20:15 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -1797,7 +1797,39 @@ export default class Counter extends React.Component {
 
 now i will chnage the code again making count to zero and rewrriing the updated incremen and decementn logic 
 
+ import React from "react";
+export default class Counter extends React.Component {
 
+    constructor(props)
+    {
+        super(props);
+        this.handleAttack = this.handleAttack.bind(this);
+        this.handleDefence = this.handleDefence.bind(this);
+        this.state = {
+            count:0
+        }
+    }
+
+    handleAttack()
+    {
+      
+        this.setState({count:this.state.count+1})
+    }
+    handleDefence()
+    {
+       
+        this.setState({ count:this.state.count-1})
+    }
+    render() {
+        return (
+            <div className="row text-white">
+                <h1>Counter:{this.state.count} </h1>
+                <button  onClick={this.handleAttack} style={{ width: "200px" }}>+1</button>
+                <button onClick={this.handleDefence} style={{ width: "200px" }}>-1</button>
+            </div>
+        )
+    }
+}
 
 
 
