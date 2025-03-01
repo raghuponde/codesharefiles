e7a756a0eb46f96ccac13c638bae93d74ae7c1ff
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 08:56:25 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -1,1 +1,8 @@
+Now i am opening yesterdays program only of MVCDemo1 so you also open yesterdays project as i am adding some small code in it 
 
+Now what i want to do is in a single view means in a single page of design i want to use muliple models earleir i used either single model or colllection of same model 
+but i have not used multiple models in the same page of .cshmtl so let us do that 
+
+There will be situation where for generating reports you may need data from multiple models as all data is not present in single model .
+
+  
