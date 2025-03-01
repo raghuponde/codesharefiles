45666859e62d9a4d13cde5de559a82ae47d8b7f8
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-01 09:07:31 +0530

    Update Phase2Day2CodeShare.cs

--- Phase2Day2CodeShare.cs
+++ Phase2Day2CodeShare.cs
@@ -7,4 +7,25 @@ There will be situation where for generating reports you may need data from mult
 
   write a actionmethod in Home controller again 
 
+  on top of the action method first deine Deparment collection like this 
   
+ List<Department> deptlist = new List<Department>()
+     {
+         new Department{Deptid=10,DeptName="Sales"},
+         new Department{Deptid=20,DeptName="HR"},
+         new Department{Deptid=30,DeptName="Software"}
+     };
+
+ public IActionResult EmpDeptViewModelDemo(int empid)
+ {
+     var query1 = deptlist.ToList();
+     Employee emp=emplist.Where(x=>x.EmployeeID == empid).FirstOrDefault();
+     var query2 = emp;
+
+     return View(query1,query2);
+
+ }
+
+Above i want to send two models one is collection of depts and another is single empooyee based on what i pass empid 
+
+but above code is givng error means i cannot pass multiple object to view from the cotroller so what to do now ????
