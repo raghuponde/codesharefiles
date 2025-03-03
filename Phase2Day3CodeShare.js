68ed750809df2b73b15fb22fa0a1c1969aa6a38f
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-03 09:13:54 +0530

    Update Phase2Day3CodeShare.js

--- Phase2Day3CodeShare.js
+++ Phase2Day3CodeShare.js
@@ -1,1 +1,16 @@
 
+Redux in React
+Redux is a state management library for JavaScript applications, commonly used with React. It helps manage application state in a predictable manner by using a centralized store.
+
+Why Use Redux?
+React’s built-in state management works well for small applications, but as the app grows, managing shared state between multiple components can become complex. Redux provides:
+
+A single source of truth (centralized state)
+Predictable state updates using pure functions (reducers)
+Easier debugging with tools like Redux DevTools
+Scalability for large applications
+
+
+All of crete a new app in your day 3 folder name reduxopedia and do intial set up means in public copy logo pics into images folder means create folder images in src and move the images 
+over there and keep index.js and css and app.js and css files and package json files keepand remaining all delete it 
+
