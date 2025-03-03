353297f60a7500968e1bd97f975ded2b59d16037
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-03 10:45:38 +0530

    Update Phase2Day3CodeShare.js

--- Phase2Day3CodeShare.js
+++ Phase2Day3CodeShare.js
@@ -417,6 +417,51 @@ function Counter() {
 
 export default Counter;
 
+Now i want to import action methods of counterSlcie as well so Counter.js 
+
+updated code 
+
+Counter.js 
+------------
+ import React from "react";
+import { useSelector,useDispatch } from "react-redux";
+import { increment,decrement } from "../redux/slice/counterSlice";
+function Counter() {
+    const count = useSelector((state) => state.counterStore.count);
+    const dispatch = useDispatch();
+    return (
+        <div
+            className=" mt-2 pt-3 pl-2 text-center"
+            style={{ borderTop: "1px solid #999" }}
+        >
+            <div className="text-white pb-2 h4">Counter : {count}</div>
+            <div className="row">
+                <div className=" p-4 col-12 col-md-6">
+                    <div className="border p-4">
+                        <button
+                            className="btn btn-primary"
+
+                            onClick={()=>dispatch(increment())}
+
+                        >
+                            Add
+                        </button>{" "}
+                        &nbsp;
+                        <button
+                            className="btn btn-danger"
+                            onClick={()=>dispatch(decrement())}
+
+                        >
+                            Remove
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Counter;
 
  
 
