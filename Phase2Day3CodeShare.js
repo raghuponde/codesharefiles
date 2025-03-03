6bde63f20d7c204e01f6f798b74d6d44f2f2d24c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-03 14:41:44 +0530

    Update Phase2Day3CodeShare.js

--- Phase2Day3CodeShare.js
+++ Phase2Day3CodeShare.js
@@ -997,6 +997,33 @@ now add another file useFetch.js into compoennts foldr
 and logic for usefecth.js 
 ---------------------------
 
+import React, { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+            fetch(url)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Failed to fetch users");
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    setData(data);
+                    setLoading(false);
+                })
+                .catch((err) => {
+                    setError(err.message);
+                    setLoading(false);
+                });
+        }, [url]);
+
+    return { data ,loading,error}
+}
 
  
 
