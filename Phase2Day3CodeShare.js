5ae8c2ba2892f94c25760aa0dc40eb0b5680cbae
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-03 12:05:50 +0530

    Update Phase2Day3CodeShare.js

--- Phase2Day3CodeShare.js
+++ Phase2Day3CodeShare.js
@@ -645,5 +645,54 @@ function Counter() {
 export default Counter;
 
 
+in the slice folder create a new file destinationSlice.js file and code  is like this 
+
+destinationSlice.js 
+--------------------
+ import { createSlice } from "@reduxjs/toolkit";
+
+const initalState = () => {
+  return {
+    destinations: [
+      {
+        name: "Hong Kong",
+        days: 7,
+        fact: "World's longest covered escalator",
+      },
+      {
+        name: "Japan",
+        days: 10,
+        fact: "Japan is mostly mountains",
+      },
+      {
+        name: "New Zealand",
+        days: 14,
+        fact: "Last country in the world to be inhabited by humans",
+      },
+    ],
+  };
+};
+
+export const destinationSlice = createSlice({
+  name: "destination",
+  initialState: initalState,
+  reducers: {},
+});
+
+export const destinationReducer = destinationSlice.reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
