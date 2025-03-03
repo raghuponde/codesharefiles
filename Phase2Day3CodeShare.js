b42e4c34ba5970a82252bfd44dd7533fe10392b0
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-03 09:23:28 +0530

    Update Phase2Day3CodeShare.js

--- Phase2Day3CodeShare.js
+++ Phase2Day3CodeShare.js
@@ -35,4 +35,34 @@ REDUXOPEDIA
 │── README.md
 
 
+index.js 
+-----------
+  import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+App.js 
+---------
+  
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
 
