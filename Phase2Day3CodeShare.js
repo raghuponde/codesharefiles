eedb436d9ee76e12b41dfdbcd0f15038983b70e6
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-03 10:30:42 +0530

    Update Phase2Day3CodeShare.js

--- Phase2Day3CodeShare.js
+++ Phase2Day3CodeShare.js
@@ -345,11 +345,9 @@ Counter.js
 ------------
 import React from "react";
 import { useSelector } from "react-redux";
-
 function Counter() {
-  const count = useSelector((state) => state.counterStore.count);
-
-  return <div>{count}</div>;
+    const count = useSelector((state) => state.counterStore.count);
+    return <div style={{color:"white"}}>{count}</div>;
 }
 
 export default Counter;
