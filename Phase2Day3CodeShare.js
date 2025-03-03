86f88b9a471dfe1abf6cef78a196b891b9e5a618
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-03 14:27:51 +0530

    Update Phase2Day3CodeShare.js

--- Phase2Day3CodeShare.js
+++ Phase2Day3CodeShare.js
@@ -958,9 +958,39 @@ const UserList = () => {
 };
 
 export default UserList;
+ index.js file 
+------------
+ import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import Header from './components/Header';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import Counter from './components/Counter';
+import DestinationList from './components/DestinationList';
+import DestinationFact from './components/DestinationFact';
+import UserList from './components/UserList';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+ 
+      <Header />
+      <Counter />
+      <DestinationList />
+      <DestinationFact />
+      <UserList/>
+    </Provider>
+  </React.StrictMode>
+);
+
 
 Problem: The fetching logic is inside the component, making it harder to reuse.
 
 Refactored Code (Using a Custom Hook useFetch)
 Now, we extract the data fetching logic into a separate function called useFetch.
+
+
 
