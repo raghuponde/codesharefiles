51d4e1927fa46b14e82ed80405806bb695d3ac41
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-04 16:00:21 +0530

    Update Phase2Day4CodeShare.cs

--- Phase2Day4CodeShare.cs
+++ Phase2Day4CodeShare.cs
@@ -1069,14 +1069,84 @@ openn a new windows appliation of .net core not framework and desing the interfc
 in the form load event i want to take student table into datatable class 
 
   so in Form  load event code is like this 
+and other button clik codes are like this 
 
-  
-now clik the buton of << and start writing the whole code which i provide here also add depencies of latest .net core of system.data.sqlclient 
+using System.Data;
+using System.Data.SqlClient;
+namespace navigatingtabledemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
 
+        DataRow dr;
+        int rn;
+        SqlConnection cnn = new SqlConnection(@"data source=LAPTOP-4G8BHPK9\SQLEXPRESS;initial catalog=Wipro4;Integrated Security=true;");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rn = 0;
+            dr = dt.Rows[rn];
+            textBox1.Text = dr[0].ToString();
+            textBox2.Text = dr[1].ToString();
 
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter ad = new SqlDataAdapter("select * from student", cnn);
+            ad.Fill(ds, "ss");
+            dt = ds.Tables["ss"];
+        }
 
-  
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rn = rn + 1;
+            if (rn <= dt.Rows.Count - 1)
+            {
+                dr = dt.Rows[rn];
+                textBox1.Text = dr[0].ToString();
+                textBox2.Text = dr[1].ToString();
+
+            }
+            else
+            {
+                rn = rn - 1;
+                MessageBox.Show("end of the record ..");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rn = rn - 1;
+            if (rn >= 0)
+            {
+                dr = dt.Rows[rn];
+                textBox1.Text = dr[0].ToString();
+                textBox2.Text = dr[1].ToString();
+
+            }
+            else
+            {
+                rn = rn + 1;
+                MessageBox.Show("end of the record ..");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            rn = dt.Rows.Count - 1;
+            dr = dt.Rows[rn];
+            textBox1.Text = dr[0].ToString();
+            textBox2.Text = dr[1].ToString();
+        }
+    }
+}
+
       
 
 
