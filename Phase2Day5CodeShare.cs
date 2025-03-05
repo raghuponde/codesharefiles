0179b26e9cb1b360cba1a1f05b33cec9c7514c8f
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-05 09:41:48 +0530

    Update Phase2Day5CodeShare.cs

--- Phase2Day5CodeShare.cs
+++ Phase2Day5CodeShare.cs
@@ -1,1 +1,19 @@
+ENTITY FRAMEWORK
+*****************
+https://www.tutorialspoint.com/entity_framework/index.htm
 
+https://www.entityframeworktutorial.net/entityframework6/what-is-entityframework.aspx
+
+
+from the above two links only take theoretical knowledge And understand the concepts and its architecture of entity framework
+
+
+now open visual studi 2022 and add new asp.net core mvc project here let it be .net core 8.0 version only 
+
+right clik on dependencies and manage nugget packages and go to browse and type this packages and install  it in your application 
+
+which version i will tell wait 
+
+Microsoft.EntityFrameworkCore  
+Microsoft.EntityFrameworkCore.SqlServer
+Microsoft.EntityFrameworkCore.Tools
