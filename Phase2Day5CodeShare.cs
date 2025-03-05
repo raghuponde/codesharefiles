fbfd8f18739b5bec08e2afa4b658a57fff4a12be
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-05 09:47:57 +0530

    Update Phase2Day5CodeShare.cs

--- Phase2Day5CodeShare.cs
+++ Phase2Day5CodeShare.cs
@@ -7,9 +7,11 @@ https://www.entityframeworktutorial.net/entityframework6/what-is-entityframework
 
 from the above two links only take theoretical knowledge And understand the concepts and its architecture of entity framework
 
+DBFirst apprach i am doing it 
+---------------------------------
 
 now open visual studi 2022 and add new asp.net core mvc project here let it be .net core 8.0 version only 
-
+ 
 right clik on dependencies and manage nugget packages and go to browse and type this packages and install  it in your application 
 
 which version i will tell wait so  cann use laetst one 9.0.2 if any issues comes i will change the version as i am using latest 8.0 core so  here in entity framework also 
@@ -20,3 +22,6 @@ Microsoft.EntityFrameworkCore
 Microsoft.EntityFrameworkCore.SqlServer
 Microsoft.EntityFrameworkCore.Tools
 
+and then in package manager console fire this command 
+
+Scaffold-DbContext 'Data Source=LAPTOP-4G8BHPK9\SQLEXPRESS;initial catalog=NORTHWND;Integrated Security=true;' Microsoft.EntityFrameWorkCore.SqlServer -OutputDir Models
