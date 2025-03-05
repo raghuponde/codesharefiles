d67f42e02d9c21492c5dac0a9446394d0a05f79d
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-05 14:28:04 +0530

    Update Phase2Day5CodeShare.cs

--- Phase2Day5CodeShare.cs
+++ Phase2Day5CodeShare.cs
@@ -617,6 +617,94 @@ Add this postcontroller with read and write actions
 
 Where a boilerplate code will come and I will modify by going inside that boilerplate code
 
+which will look like tis 
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkDemo1.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: PostController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PostController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PostController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
+now Updated PostContoller 
+---------------------------
 
 
 
