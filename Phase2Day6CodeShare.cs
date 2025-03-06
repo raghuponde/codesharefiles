2a984a70dcaaa2580ae44e11b425747bbb172f9f
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-06 14:27:30 +0530

    Update Phase2Day6CodeShare.cs

--- Phase2Day6CodeShare.cs
+++ Phase2Day6CodeShare.cs
@@ -1248,6 +1248,63 @@ Earlier in DB first approach and code first approach any context class which I a
      using Microsoft.EntityFrameworkCore;
 	 
 	 
-	
+so my program.cs lookslike this
+
+program.cs 
+--------------
+using Microsoft.AspNetCore.Identity;
+using SecureAppDemo.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SecureAppDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add Identity services to the container
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                // Configure password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>() // Use your ApplicationDbContext here
+                .AddDefaultTokenProviders();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
+
   
   
