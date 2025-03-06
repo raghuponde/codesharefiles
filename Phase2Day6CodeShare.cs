a2bd7caec9cbc69cb5f579f2c5307ed690c7b8f6
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-06 09:01:56 +0530

    Update Phase2Day6CodeShare.cs

--- Phase2Day6CodeShare.cs
+++ Phase2Day6CodeShare.cs
@@ -1,1 +1,15 @@
 
+Code First Approach
+***********************
+The C# classes (models) are created first.
+EF generates the corresponding database and tables based on these classes.
+Uses Migrations to update the database schema.
+
+  When to use?
+
+---->When you want full control over your database structure.
+---->When starting a new project from scratch.
+
+  you will only generate the database from the front end into the database server 
+  means you will follow some steps to do that 
+  
