fad0b1d4fdefb3fea7b8e5289ec44ee3b621d512
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-06 10:09:25 +0530

    Update Phase2Day6CodeShare.cs

--- Phase2Day6CodeShare.cs
+++ Phase2Day6CodeShare.cs
@@ -204,8 +204,132 @@ tools--->Nugget package manager --->package manager console
 add-migration 'initaldbcreated'    (after this command a migration folder will be created telling how it is going to create db tables ) 
 update-database 
 
-
+ Now i want to add some more classes into the same program but with annotations which will provide valdiation to me according constraints will be imposed on the table from 
+ the classe and I also want to impose fleunt api to provide realtionship .
  
+ so some classes i will be adding and again same commands i will be using 
+ 
+ so now add classes Author1 ,Course1 ,Employee ,UserDetail into the models folder 
+ 
+ 
+ namespace codefirstentityframeworkdemo.Models
+{
+    public class Author1
+    {
+
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public IList<Course1> Courses { set; get; }
+    }
+}
+
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace codefirstentityframeworkdemo.Models
+{
+    public class Course1
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { set; get; }// not an identity column 
+
+        [Required]
+        [Column("Stitle", TypeName = "varchar")]
+        public string Title { set; get; }
+
+        [Required]
+        [MaxLength(220)]
+        public string Description { set; get; }
+
+
+        public float fullprice { set; get; }
+
+
+        [ForeignKey("Author")]
+        public int AuthorId { set; get; }
+
+        public Author1 Author { set; get; }
+
+    }
+}
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace codefirstentityframeworkdemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your firstname")]
+        public string? FirstName { set; get; }
+
+        [Required(ErrorMessage = "Please enter your lastname")]
+        public string? LastName { set; get; }
+
+        [Required(ErrorMessage = "Please enter email id")]
+        [EmailAddress(ErrorMessage = "Please enter valid email id")]
+        public string? Email { set; get; }
+
+        [Required(ErrorMessage = "Please enter your age")]
+        [Range(0, 100, ErrorMessage = "Please enter your age betwen 1 to 100 only ")]
+
+        public int Age { set; get; }
+    }
+}
+
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace codefirstentityframeworkdemo.Models
+{
+    public class UserDetail
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "User Name is Required")]
+        [StringLength(15, ErrorMessage = "User Name cannot be more than 15 characters")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        [StringLength(11, MinimumLength = 5, ErrorMessage = "Minimum Length of Password is 5 letters or Max Length is of 11 letters..")]
+        [DataType("password")]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Date Of Birth is Required")]
+        [DisplayName("Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress(ErrorMessage = "Please enter valid Email Id")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is Required")]
+        [Range(100, 1000, ErrorMessage = "Must be between 100 and 1000")]
+        public int PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is Required")]
+        [DisplayName("Phone Number")]
+        public int PhoneNo { get; set; }
+
+        [Required(ErrorMessage = "Profile is Required")]
+        [DataType(DataType.MultilineText)]
+        public string Profile { get; set; }
+    }
+}
+
 
 
   
