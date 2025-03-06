ef3289bb8199796b639c1eb1222ae6aaedc026a6
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-06 09:08:37 +0530

    Update Phase2Day6CodeShare.cs

--- Phase2Day6CodeShare.cs
+++ Phase2Day6CodeShare.cs
@@ -12,6 +12,28 @@ Uses Migrations to update the database schema.
 
   you will only generate the database from the front end into the database server 
   means you will follow some steps to do that 
+code first approach steps :
+---------------------------
+1)Installing packages core package ,tools,sql server 9.0.2 version install these dependencies 
+     Microsoft.EntityFrameworkCore
+    Microsoft.EntityFrameworkCore.SqlServer
+    Microsoft.EntityFrameworkCore.Tools
+2)creating classes in Models folder which will later will be converted to db tables as i am using code first apprach 
+
+3)context class also u need to create and in that u have to add all classes 
+   and set the path in app  settings file and also in program.cs file also inject the dependency 
+
+4)Build the application 
+
+5)add migrations to convert your classes into table in package manager console..
+    add-migration 'anytaskname'
+    update-database 
+    
+    again any change you are doing in classes in the fron end again add those above commands 
+    if u are not happy with the migrations which u have done by mistake u can delete migrations folder which 
+    will be automaticcally generated and again u can run same above two commands
+
+so now let us open one new application and start the code first approach 
 
   Create a new application with the name codefirstentityframeworkdemo in day 6 folder It is aasp.net core mvc application
   use .net core 8.0 version as usual 
