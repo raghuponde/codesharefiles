0d2f8d4555b6248b85371d43f62191e06553aab3
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-08 09:57:46 +0530

    Update Phase2Day8CodeShare.js

--- Phase2Day8CodeShare.js
+++ Phase2Day8CodeShare.js
@@ -136,3 +136,19 @@ export default TaskList
 Now to to TaskCard.js 
 ---------------------
  
+import React, { useContext } from 'react'
+import { TaskContext } from './TaskContext'
+
+export default function TaskCard(task) {
+   const { deleteTask}= useContext(TaskContext);
+    return (
+
+        <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
+
+
+            <span>{task.id} -- {task.name}</span>
+            <button className='delete' onClick={() => deleteTask(task.id)} >Delete</button>
+        </li>
+
+    )
+}
