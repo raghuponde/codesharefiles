0cdc0fcf218ab1b0b9b68c2d834ae998358e9152
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-08 09:40:41 +0530

    Update Phase2Day8CodeShare.js

--- Phase2Day8CodeShare.js
+++ Phase2Day8CodeShare.js
@@ -38,7 +38,45 @@ to TaskCard as well
 so i want to do this using createContext and useContext i dont want to use props here 
 
 Now in the components folder add TaskContext.js file 
+TaskContext.js
+--------------
+ import { createContext, useState } from "react";
 
+//create a context
+
+export const TaskContext = createContext();
+
+// provider component
+
+export const TaskProvider = ({ children }) => {
+    
+    const [tasks, setTasks] = useState([
+        { id: 5271, name: "Record React Lectures", completed: true },
+        { id: 7825, name: "Edit React Lectures", completed: false },
+        { id: 8391, name: "Watch Lectures", completed: false }
+      ]);
+    
+    // function to add a new task
+    const addTask = (task) =>
+    {
+        setTasks([...tasks, task]);
+    }
+
+    // funtion to delete a task
+
+    const deleteTask = (id) =>
+    {
+        setTasks(tasks.filter((task) => task.id !== id));
+    }
+
+    return (
+
+        <TaskContext.Provider value={{ tasks, addTask, deleteTask }}>
+            children
+        </TaskContext.Provider>
+    )
+
+}
 
 
 
