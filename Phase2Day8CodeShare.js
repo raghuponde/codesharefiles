a8adf1348bc4dcd04885f356458aec3f7086c3d4
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-08 09:20:22 +0530

    Update Phase2Day8CodeShare.js

--- Phase2Day8CodeShare.js
+++ Phase2Day8CodeShare.js
@@ -2,7 +2,37 @@ Just check where i had used Use context programming Phase2Day4CodeShare.cs file 
 https://drive.google.com/drive/folders/1AKrv_IbdPOVkZJsZ9W1ESrUkK5lHnZ7c?usp=sharing
 there once go through ContextApi and createcontext and useContext demos 
 
-just analize 
+just analize the authcontext code there 
+
+Earlier we have used props to transfer properties or some information from parent component to child component
+ the same thing I can do with using createContext and useContext hooks from react 
+Here you have to first create the context where certain properties or functions you are exporting to all the child components 
+ so here I will use create context hook for creating a context Then then then immediately what context you have created create that context provider 
+  and export the values and you have to configure the auth provider in the app js as a main parent
+  component so that all child components can take the properties and functionalities of Oregon the context class which you have created
+
+<anyname>Context=createContext
+
+<anyname>Provider = ({ children })
+
+in return 
+<anyname>ContextProvider.value export it 
+
+
+so while creating context and 
+
+who is using that context 
+
+impor the file
+and will use useContext(<anyname>Context)
+
+so will take the proeprties and and display 
+
+Now create  one react app with the name propusagedemo 
+
+
+
+
 
 
 
