299d429c5dc24d199fa204bf4ff417e7cf89b290
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-10 15:02:16 +0530

    Update Phase2Day9CodeShare.js

--- Phase2Day9CodeShare.js
+++ Phase2Day9CodeShare.js
@@ -270,6 +270,90 @@ next add AuthController in controller folder of empty of api type only add it
 
  }
 
+so righ now AuthControllr will look like tis complete code 
+
+using IdentityDemowithTokeninCore.Models.Authentication.SignUp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using IdentityDemowithTokeninCore.Models;
+
+namespace IdentityDemowithTokeninCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+
+            _configuration = configuration;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            //Check User Exist 
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response { Status = "Error", Message = "User already exists!" });
+            }
+
+            //Add the User in the database
+            IdentityUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.Username
+
+            };
+            if (await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _userManager.CreateAsync(user, registerUser.Password);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "User Failed to Create" });
+                }
+                //Add role to the user....
+
+                await _userManager.AddToRoleAsync(user, role);
+
+
+
+
+
+                return StatusCode(StatusCodes.Status200OK,
+                    new Response { Status = "Success", Message = $"User created SuccessFully" });
+
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "This Role Doesnot Exist." });
+            }
+
+
+        }
+
+
+    }
+}
 
 
 
