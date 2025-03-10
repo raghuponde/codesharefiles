9e19e75adc2e98cc07433c20b8e97c5ad42d8338
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-10 09:29:41 +0530

    Update Phase2Day9CodeShare.js

--- Phase2Day9CodeShare.js
+++ Phase2Day9CodeShare.js
@@ -2,8 +2,6 @@ first create three folders like this one is Boilerplatecode ,passedcodeinlocal,m
 
 and after this open the notes of saturday ...code share notes to keep it side by  handy 
 
-so copy the downloads of milsteonquestionssample of the folder boilerplatecode and boilerplatewrittencode into fodler boilerplatecode in day 9 and written
-in chatgpt fodler just copy the contents 
+so copy the downloads of milsteonquestionssample of the folder boilerplatecode and boilerplatewrittencode and passedcodeinlocal into fodler boilerplatecode in day 9 and written
+in chatgpt fodler and passedccodeinlocal2 copy it 
 
-and now one folder is left out which is passed code so in day8 Milestonesolution folder content put it by donwlaodng 
-open all folders in vscode and everywhere type npm install 
