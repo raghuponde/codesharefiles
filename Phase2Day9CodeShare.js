6d007a92e6d271c552915b4c45fedcfdca902870
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-10 09:09:29 +0530

    Update Phase2Day9CodeShare.js

--- Phase2Day9CodeShare.js
+++ Phase2Day9CodeShare.js
@@ -1,1 +1,8 @@
+first create three folders like this one is Boilerplatecode ,passedcodeinlocal,mycodechatgpt
 
+and after this open the notes of saturday ...code share notes to keep it side by  handy 
+
+so copy the downloads of milsteonquestionssample of the folder boilerplatecode and boilerplatewrittencode into fodler boilerplatecode in day 9 and written
+in chatgpt fodler just copy the contents 
+
+and now one folder is left out which is passed code so in day8 Milestonesolution folder content put it by donwlaodng 
