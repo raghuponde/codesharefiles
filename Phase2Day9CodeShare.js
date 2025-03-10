d7de0a7925265c8e0ca18493d9a70aee7fad25aa
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-10 12:55:28 +0530

    Update Phase2Day9CodeShare.js

--- Phase2Day9CodeShare.js
+++ Phase2Day9CodeShare.js
@@ -27,5 +27,40 @@ Microsoft.AspNetCore.Identity.EntityFrameworkCore
 Microsoft.AspNetCore.Authentication.JwtBearer
 
 
+after installing this once build the project once
+add one folder into the project with the name Models in that add Authentication folder and in
+that add one Login and SignUp folder again
+
+Solution 'User.Management.API' (2 of 2 projects)
+└── User.Management.API
+    ├── Connected Services
+    ├── Dependencies
+    ├── Properties
+    ├── Controllers
+    ├── Migrations
+    ├── Models
+    │   ├── Authentication
+    │   │   ├── Login
+    │   │   │   └── LoginModel.cs
+    │   │   ├── SignUp
+    │   │   │   └── RegisterUser.cs
+    │   ├── ApplicationDbContext.cs
+    │   ├── Response.cs
+    ├── .editorconfig
+    ├── appsettings.json
+    ├── GlobalSuppressions.cs
+    ├── Program.cs
+└── User.Management.Service
+    ├── Dependencies
+    ├── Models
+    │   ├── EmailConfiguration.cs
+    │   ├── Message.cs
+    ├── Services
+    │   ├── GlobalSuppressions.cs
+
+
+
+then create a class with the name RegisterUser.cs 
+
 
 
