527cb9297bacad3a5da81faddf2cd421b6967c86
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-10 09:45:52 +0530

    Update Phase2Day9CodeShare.js

--- Phase2Day9CodeShare.js
+++ Phase2Day9CodeShare.js
@@ -9,3 +9,7 @@ now do npm install in local2 and chatgpt folder
 
 now copy the content of boiler plate App.test.js into both chatgpt and also locla2 fodler and run npm test 
 
+you can see that local all test cases are passing but chatgpt it is not passing eventhoug he has not written the code for categaory add into the task
+
+open questin description from drive or if u have local open it and then open test file of boilerplate app.test.js 
+and also open in notepad++ the open day code share file as well 
