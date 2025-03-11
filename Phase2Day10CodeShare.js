231cb34cf53bbc6ad441c6303b1d5f3dccb738dd
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-11 09:41:44 +0530

    Update Phase2Day10CodeShare.js

--- Phase2Day10CodeShare.js
+++ Phase2Day10CodeShare.js
@@ -157,5 +157,66 @@ builder.Services.AddCors(options =>
 
 app.UseCors("AllowAllOrigins");(this after app.UseHttpsRedirection();)
 
+so my program.cs file in total will look like this 
+
+program.cs
+------------
+  
+using Microsoft.EntityFrameworkCore;
+using StudentReactWebApIDemo.Data;
+
+namespace StudentReactWebApIDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowAllOrigins");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
 
 
