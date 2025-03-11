88f5ac8fe4b83cf515408feb4718d60fd7bab954
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-11 09:21:00 +0530

    Update Phase2Day10CodeShare.js

--- Phase2Day10CodeShare.js
+++ Phase2Day10CodeShare.js
@@ -1,1 +1,163 @@
+Integratng .net core WebApi with react Demo
+*********************************************
+  create a new project with the name StudentReactWebApIDemo it is an web api project which we are doing 
+and include below namespaces 6.0.33 version you have to use 
+
+ Microsoft.EntityFrameworkCore
+ Microsoft.EntityFrameworkCore.SqlServer
+ Microsoft.EntityFrameworkCore.Tools
+
+Add one class in Models folders so create one foldr Models 
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+}
+
+Now add one folder Data in project and add class AppDbConext which is like this below
+
+ using Microsoft.EntityFrameworkCore;
+    using StudentReactWebApIDemo.Models;
+namespace StudentReactWebApIDemo.Data
+{
+   
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+
+}
+
+
+in ap settings configure database 
+
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft.AspNetCore": "Warning"
+    }
+  },
+  "AllowedHosts": "*",
+  "ConnectionStrings": {
+    "DefaultConnection": "Server=LAPTOP-4G8BHPK9\\SQLEXPRESS;Database=StudentCRUDDB;Trusted_Connection=True;MultipleActiveResultSets=true"
+  }
+
+
+}
+
+udate in program.cs 
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    
+    
+    
+    
+addd migrations 
+
+Add-Migration InitialCreate
+Update-Database
+
+Now create a StudentsController and below code is presented use empty api contoller only 
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public StudentsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+    {
+        return await _context.Students.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Student>> GetStudent(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+        return student;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Student>> PostStudent(Student student)
+    {
+        _context.Students.Add(student);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutStudent(int id, Student student)
+    {
+        if (id != student.Id)
+        {
+            return BadRequest();
+        }
+        _context.Entry(student).State = EntityState.Modified;
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Students.Any(e => e.Id == id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteStudent(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+        _context.Students.Remove(student);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
+
+
+o allow access from the React frontend, enable CORS in Program.cs:(after   builder.Services.AddSwaggerGen();)
+--------------------------------------------------------------
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+app.UseCors("AllowAllOrigins");(this after app.UseHttpsRedirection();)
+
+
 
