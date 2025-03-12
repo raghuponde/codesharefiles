b7559be83fe7316dee8d977e93fb7ab3e9a0eda0
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-12 08:59:23 +0530

    Update Phase2Day11CodeShare.js

--- Phase2Day11CodeShare.js
+++ Phase2Day11CodeShare.js
@@ -1,1 +1,32 @@
+The important points 
+---------------------------
+using useSate u have to create controlled components in the design 
+
+using props u have to pass values from dashboard to child components which it is holding here they are not using router if u keep sub elelemts in main container and if i show that main 
+container which is nothing but dashboard then no need of router 
+
+every component check what it is taking from service and that only u implment it there and pass as context sometimes all it will not export only few it will export and some to other components okay 
+
+first go and write reducer code in service file and then go with context 
+
+in components first desing u do it and later on write other codes 
+
+check test file and according  to  that do desing 
+
+In the dashboard check what components has to be displayed and from dashboad u will only have to send it by props and go to components and see { what they are taking that only send it in dashboard }
+
+make your familair with writing arrow function and map and filter which are used extensively here 
+
+
+
+
+In category list u are doing both adding and deleting 
+
+
+but  in Tasklist for adding taskinput.js is there and for  and for siaplying tasklist it can be divided 
+
+you should know how to toggle the design  using useState and it done using ternary operator ? true means first value or secod value 
+
+but when it is null value in useState it is reverse means if   ? true means not null so firt will be displayed otherwise second will be desilayed 
+
 
