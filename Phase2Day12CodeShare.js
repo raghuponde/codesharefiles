e74dfc79145469090e5d6702bee56fadc0dc605a
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-13 09:54:50 +0530

    Update Phase2Day12CodeShare.js

--- Phase2Day12CodeShare.js
+++ Phase2Day12CodeShare.js
@@ -1,6 +1,3 @@
 
-Go to day 9 folder and in that solving milstone folder and in that download preptestboilerplatecodeinlcoal
 
-and then open in vscode and say npm install at the terminal to install node modules and also install 
 
-npm i react-router-dom@6.4.3  at the terminal 
