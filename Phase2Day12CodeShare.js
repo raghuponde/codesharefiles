4c7ba54402b2e76abea583385453804275708a0c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-13 12:55:42 +0530

    Update Phase2Day12CodeShare.js

--- Phase2Day12CodeShare.js
+++ Phase2Day12CodeShare.js
@@ -705,6 +705,144 @@ Suresh   Suresh#123  User
 
 Sita   Sita#123     HR 
 
+now on students controller already Authorize is there now agin what i will do is edit ,delete and insert on student only Admin can do like that 
+i will put roels on top of them like ths shown below 
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentReactWebApIDemo.Data;
+using StudentReactWebApIDemo.Models;
+
+namespace StudentReactWebApIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,User,HR")]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Admin,User,HR")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<Student>> PostStudent([FromForm] StudentDto studentDto)
+        {
+            var student = new Student
+            {
+                Name = studentDto.Name,
+                Email = studentDto.Email,
+                Address = studentDto.Address
+            };
+
+            if (studentDto.ImageFile != null)
+            {
+                var filePath = Path.Combine("wwwroot/images", Guid.NewGuid() + Path.GetExtension(studentDto.ImageFile.FileName));
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await studentDto.ImageFile.CopyToAsync(stream);
+                }
+                student.ImageUrl = filePath.Replace("wwwroot", "");
+            }
+
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> PutStudent(int id, [FromForm] StudentDto studentDto)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            student.Name = studentDto.Name;
+            student.Email = studentDto.Email;
+            student.Address = studentDto.Address;
+
+            if (studentDto.ImageFile != null)
+            {
+                var filePath = Path.Combine("wwwroot/images", Guid.NewGuid() + Path.GetExtension(studentDto.ImageFile.FileName));
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await studentDto.ImageFile.CopyToAsync(stream);
+                }
+                student.ImageUrl = filePath.Replace("wwwroot", "");
+            }
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+
+    }
+}
+
+so here first login as HR and try to udpate any student it will  not allow 
+then try to read get and get students it will allow 
+
+same when  u login as admin  u can do it now in all this process tokens will be created for admin he will be having 
+his own token and HR also his own like that it goes 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
