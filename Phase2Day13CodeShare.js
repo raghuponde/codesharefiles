d562ffb0b7e7593af60084880aaf36fb7b934519
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-14 14:47:37 +0530

    Update Phase2Day13CodeShare.js

--- Phase2Day13CodeShare.js
+++ Phase2Day13CodeShare.js
@@ -1,2 +1,59 @@
 Go to the project which i had done yesterday Day12  on web api implementing identity with token and also take the front end from day 10 and try to connect 
 from front end to back end 
+
+so i am getting this error 
+
+Request failed with status code 401
+AxiosError: Request failed with status code 401
+    at settle (http://localhost:3000/static/js/bundle.js:2186:12)
+    at XMLHttpRequest.onloadend (http://localhost:3000/static/js/bundle.js:823:66)
+    at Axios.request (http://localhost:3000/static/js/bundle.js:1322:41)
+ERROR
+Request failed with status code 401
+AxiosError: Request failed with status code 401
+    at settle (http://localhost:3000/static/js/bundle.js:2186:12)
+    at XMLHttpRequest.onloadend (http://localhost:3000/static/js/bundle.js:823:66)
+    at Axios.request (http://localhost:3000/static/js/bundle.js:1322:41)
+
+I will not get this error if in the back end I remove authorised attribute So so now If to access with Authorize attibute controlelr u 
+  have to write a  code or design for Auth  Controller I am having design for student controller only 
+
+so go to chatgpt and tell 
+
+for student controler in react i am mainting this code 
+
+---->paste StudentService code with heading so that it knows what you are typing now 
+
+---->Paste next studnet form and studentlist and App.js codes 
+
+after pasting all code with proper headings write like this 
+
+so this react code for the student web api now for this which u have given Authcontroller  i want code for that in react and it should connect studnet controller so i shoudl be able to logn and do register and in servcie chaneg the code as per authetication and authorization
+
+Here AuthContoller code i had done by taking the help of chat gpt so i had written like that 
+
+otherwise what u can do is 
+
+  or 
+
+Take all web api code including the fron end react code paste it and tell that for AuthContoller in the web api i want react code for that which is not 
+there it should connect studnet controller so i shoudl be able to logn and do register and in service change the code as per authetication and authorization
+
+Then chatgpt some ouptut put it and execute the code 
+
+so now i am putting that code one by one and i will explain that code 
+
+so u can see i cannot touch web api I am getting 401 error what to do let us start 
+
+
+
+
+
+
+
+
+
+
+
+            
+
