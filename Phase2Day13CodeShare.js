c3753bec9957853ffbcb88fb96a4b251e71f6205
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-03-14 14:50:47 +0530

    Update Phase2Day13CodeShare.js

--- Phase2Day13CodeShare.js
+++ Phase2Day13CodeShare.js
@@ -71,6 +71,41 @@ so let us proceed with the changes and steps to follow
 5)Protect the Student Management Pages to be accessed only by authenticated users.
 
 
+now add one class in service folder AuthService.js 
+
+1)AuthService.js (src/services/AuthService.js)
+--------------------------------------------
+import axios from 'axios';
+
+const API_URL = 'https://localhost:7272/api/Auth';
+
+class AuthService {
+    register(userData) {
+        return axios.post(`${API_URL}?role=${userData.role}`, userData);
+        // Sending role as query parameter instead of in request body
+    }
+
+    login(userData) {
+        return axios.post(API_URL + '/login', userData)
+            .then(response => {
+                if (response.data.token) {
+                    localStorage.setItem('user', JSON.stringify(response.data));
+                }
+                return response.data;
+            });
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));
+    }
+}
+
+export default new AuthService();
+
 
 
 
